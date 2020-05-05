a458f91693b7038842d87ae07fab7ea24487cb65
cctyl <2621460357@qq.com>
2020-05-05 16:05:58 +0800

    完成权限添加

--- manger-service/src/main/java/com/atguigu/scw/manger/service/TRolePermissionService.java
+++ manger-service/src/main/java/com/atguigu/scw/manger/service/TRolePermissionService.java
@@ -3,7 +3,18 @@ package com.atguigu.scw.manger.service;
 import com.atguigu.scw.manger.bean.TRolePermission;
 
 public interface TRolePermissionService {
-   public int addPermission(TRolePermission rolePermission);
+    /**
+     * 给角色添加权限
+     *
+     * @param rolePermission
+     * @return
+     */
+    public int addPermission(TRolePermission rolePermission);
 
+    /**
+     * 删除角色的某个权限
+     * @param rolePermission
+     * @return
+     */
     public int delPermission(TRolePermission rolePermission);
 }
--- manger-service/src/main/java/com/atguigu/scw/manger/service/imp/TRolePermissionServiceImpl.java
+++ manger-service/src/main/java/com/atguigu/scw/manger/service/imp/TRolePermissionServiceImpl.java
@@ -1,21 +1,49 @@
 package com.atguigu.scw.manger.service.imp;
 
 import com.atguigu.scw.manger.bean.TRolePermission;
+import com.atguigu.scw.manger.dao.TRolePermissionMapper;
+import com.atguigu.scw.manger.example.TRolePermissionExample;
 import com.atguigu.scw.manger.service.TRolePermissionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class TRolePermissionServiceImpl  implements TRolePermissionService {
+    Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    TRolePermissionMapper rolePermissionMapper;
+
+    /**
+     * 删除角色的某个权限
+     * @param rolePermission
+     * @return
+     */
     @Override
     public int delPermission(TRolePermission rolePermission) {
-        return 0;
+        logger.debug("给角色删除权限--"+rolePermission.getRoleid());
+
+        TRolePermissionExample example = new TRolePermissionExample();
+        TRolePermissionExample.Criteria criteria = example.createCriteria();
+        criteria.andRoleidEqualTo(rolePermission.getRoleid());
+        criteria.andPermissionidEqualTo(rolePermission.getPermissionid());
+
+        return rolePermissionMapper.deleteByExample(example);
     }
 
+    /**
+     * 给角色添加权限
+     * @param rolePermission
+     * @return
+     */
     @Override
     public int addPermission(TRolePermission rolePermission) {
-        return 0;
+
+        logger.debug("给角色添加权限--"+rolePermission.getRoleid());
+        return rolePermissionMapper.insert(rolePermission);
     }
 }
